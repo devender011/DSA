3de322febcc47908f5aeeb84ec9f14cea3822a9d
Devender <dipurajput011@gmail.com>
2024-10-23 19:31:10 +0530

    Update pattern3.cpp

--- pattern3.cpp
+++ pattern3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void pattern(int n){
    for(int i=1;i<=n;i++){
       for(int j=1;j<=i;j++){
-         cout<<i<<" ";
+         cout<<j<<" ";
          
       }
       cout<<endl;
