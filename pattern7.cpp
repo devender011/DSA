49ab8634646c2d30efbc471676e0c3a483dc0792
Devender <dipurajput011@gmail.com>
2024-10-26 20:35:30 +0530

    Update pattern7.cpp

--- pattern7.cpp
+++ pattern7.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 void pattern(int n){
   for(int i=0;i<=n-1;i++){
-   for(int j=0;j<=n-1-i;j++){
+   for(int j=0;j<n-1-i;j++){
       cout<<" ";
    }
    for(int j=0;j<2*i+1;j++){
       cout<<"*";
    }
-   for(int j=0;j<=n-1-i;j++){
+   for(int j=0;j<n-1-i;j++){
       cout<<" ";
    }
    cout<<endl;
