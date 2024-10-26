43330e57d09c232bdd2961aa42fa6a541c7e60e2
Devender <dipurajput011@gmail.com>
2024-10-26 19:31:12 +0530

    Create pattern7.cpp

---
+++ pattern7.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern(int n){
+  for(int i=0;i<=n-1;i++){
+   for(int j=0;j<=n-1-i;j++){
+      cout<<" ";
+   }
+   for(int j=0;j<2*i+1;j++){
+      cout<<"*";
+   }
+   for(int j=0;j<=n-1-i;j++){
+      cout<<" ";
+   }
+   cout<<endl;
+  }
+}
+int main(){
+   int t;
+   cin>>t;
+   for(int i=0;i<t;i++){
+      int n;
+      cin>>n;
+      pattern(n);
+   }
+   
+   return 0;
+}
