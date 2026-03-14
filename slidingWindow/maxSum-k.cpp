47fdeb5d4f49174aafd30581afc87ef856e0b1ea
devender011 <dipurajput011@gmail.com>
2026-03-14 22:01:15 +0530

    start slidingwindow

---
+++ slidingWindow/maxSum-k.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//find maximum subarray sum of size k
+int main() {
+    int k=2;
+    int arr[]={1,2,6,5,8};
+    int size=sizeof(arr)/4;
+    cout<<"hello world";
+
+    return 0;
+}
